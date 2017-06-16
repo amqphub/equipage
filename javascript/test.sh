fc9f5cbae5d9946bfba92b7517d6afe55f879693
Justin Ross <jross@apache.org>
2017-06-16 15:25:52 -0700

    Use my public test server

--- javascript/test.sh
+++ javascript/test.sh
@@ -2,18 +2,13 @@
 
 set -e
 
-host=127.0.0.1
-port=56720
+host=amqp.zone
+port=5672
 address=jobs
-
-qbroker --host $host --port $port &
-broker_pid=$!
-
-sleep 1
 
 node processor.js $host $port $address p1 &
 processor_pid=$!
 
-trap "kill $broker_pid $processor_pid" EXIT
+trap "kill $processor_pid" EXIT
 
 node request.js $host $port $address abc
--- python/test.sh
+++ python/test.sh
@@ -2,18 +2,13 @@
 
 set -e
 
-host=127.0.0.1
-port=56720
+host=amqp.zone
+port=5672
 address=jobs
-
-qbroker --host $host --port $port &
-broker_pid=$!
-
-sleep 1
 
 python processor.py $host $port $address p1 &
 processor_pid=$!
 
-trap "kill $broker_pid $processor_pid" EXIT
+trap "kill $processor_pid" EXIT
 
 python request.py $host $port $address abc
