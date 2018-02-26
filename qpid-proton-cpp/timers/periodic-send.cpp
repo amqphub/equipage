3cee2ac593cb8c1b35a814966da4ed9024e42ee5
Justin Ross <jross@apache.org>
2018-02-26 15:59:46 -0800

    Stop after a given number of messages

---
+++ qpid-proton-cpp/timers/periodic-send.cpp
@@ -0,0 +1,99 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+#include <proton/connection.hpp>
+#include <proton/container.hpp>
+#include <proton/duration.hpp>
+#include <proton/message.hpp>
+#include <proton/messaging_handler.hpp>
+#include <proton/sender.hpp>
+#include <proton/target.hpp>
+#include <proton/work_queue.hpp>
+
+#include <iostream>
+#include <string>
+
+struct send_handler : public proton::messaging_handler {
+    std::string conn_url_ {};
+    std::string address_ {};
+    std::string message_body_ {};
+    proton::work_queue* work_queue_;
+    proton::duration interval_ {1 * proton::duration::SECOND};
+    int desired_ {0};
+    int sent_ {0};
+
+    void on_container_start(proton::container& cont) override {
+        proton::connection conn = cont.connect(conn_url_);
+        conn.open_sender(address_);
+    }
+
+    void on_sender_open(proton::sender& snd) override {
+        std::cout << "SEND: Opened sender for target address '"
+                  << snd.target().address() << "'\n";
+
+        work_queue_ = &snd.work_queue();
+        work_queue_->schedule(interval_, [=] { send(snd); });
+    }
+
+    void send(proton::sender snd) {
+        work_queue_->schedule(interval_, [=] { send(snd); });
+
+        if (snd.credit() > 0) {
+            proton::message msg {"hello-" + std::to_string(sent_)};
+            snd.send(msg);
+
+            std::cout << "SEND: Sent message '" << msg.body() << "'\n";
+
+            sent_++;
+
+            if (sent_ == desired_) {
+                snd.close();
+                snd.connection().close();
+            }
+        }
+    }
+};
+
+int main(int argc, char** argv) {
+    if (argc != 3 && argc != 4) {
+        std::cerr << "Usage: periodic-send CONNECTION-URL ADDRESS [MESSAGE-COUNT]\n";
+        return 1;
+    }
+
+    send_handler handler {};
+    handler.conn_url_ = argv[1];
+    handler.address_ = argv[2];
+
+    if (argc == 4) {
+        handler.desired_ = std::stoi(argv[3]);
+    }
+
+    proton::container cont {handler};
+
+    try {
+        cont.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << "\n";
+        return 1;
+    }
+
+    return 0;
+}
